f644bd06294225c0f9994c5bfb12e660d3e05bbd
faizul.khan <faizul.khan@dngroup.com>
2025-04-22 14:13:42 +0600

    server.js change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, Azure DevOps1!'));
+app.get('/', (req, res) => res.send('Hello, Azure DevOps!'));
 
 // Only start the server when run directly
 if (require.main === module) {
